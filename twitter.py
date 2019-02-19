ecd65db474ba25b93aada9ea2fe275009b66bc12
Pat Myron <pmyron@nd.edu>
2019-02-18 21:56:43 -0500

    deleting duplicate commented code

--- twitter.py
+++ twitter.py
@@ -7,13 +7,5 @@ for i in range(100000):
     if "Myron" not in r.url and 'account/suspended' not in r.url:
         tweeters[r.url.split('/')[3]] += 1
         print(sorted(tweeters.items(), key=lambda item: item[1], reverse=True))
-    # r = requests.get('http://www.fb.com/' + str(i))
-    # if str(i) not in r.url:
-    #     print(r.url)
-    # r = requests.get('https://news.ycombinator.com/item?id=' + str(i))
-    # print(r.url)
-    # r = requests.get('https://www.reddit.com/comments/' + str(i))
-    # if '/r/' in r.url:
-    #     print(r.url)
     # r = requests.get('https://www.instagram.com/p/' + chr(ord('A') + i))
     # print(r.url)
