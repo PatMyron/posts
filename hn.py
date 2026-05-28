5ca57ad69f2ab278f3d9b238af3cd9e7ece85c03
Yassinbrine <Yassinbrine@users.noreply.github.com>
2026-05-28 13:24:42 +0400

    Reuse requests sessions

--- hn.py
+++ hn.py
@@ -2,12 +2,13 @@ from collections import defaultdict
 
 import requests
 
+SESSION = requests.Session()
 tweeters = defaultdict(lambda: 0)
 
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://hacker-news.firebaseio.com', None)
 for i in range(100000):
-    r = requests.get('https://news.ycombinator.com/item?id=' + str(i))
+    r = SESSION.get('https://news.ycombinator.com/item?id=' + str(i))
     tweeters[firebase.get('/v0/item/' + str(i), 'by')] += 1
     print(sorted(tweeters.items(), key=lambda item: item[1], reverse=True))
--- reddit.py
+++ reddit.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 import requests
 
+SESSION = requests.Session()
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
 }
@@ -9,8 +10,8 @@ headers = {
 tweeters = defaultdict(lambda: 0)
 
 for i in range(100000):
-    r = requests.get('https://www.reddit.com/comments/' + str(i))
+    r = SESSION.get('https://www.reddit.com/comments/' + str(i))
     if '/r/' in r.url:
-        r = requests.get('https://www.reddit.com/comments/' + str(i) + '.json', headers=headers)
+        r = SESSION.get('https://www.reddit.com/comments/' + str(i) + '.json', headers=headers)
         tweeters[r.json()[0]['data']['children'][0]['data']['author']] += 1
         print(sorted(tweeters.items(), key=lambda item: item[1], reverse=True))
